1891376b74eb7915f30b68bf6b904c45962dd3b5
joseloc16 <90774372+joseloc16@users.noreply.github.com>
2022-07-16 01:04:18 -0500

    first commit

---
+++ src/main/java/io/getarrays/server/model/Server.java
@@ -0,0 +1,13 @@
+package io.getarrays.server.model;
+
+import io.getarrays.server.enumeration.Status;
+
+public class Server {
+  private Long id;
+  private String ipAddress;
+  private String name;
+  private String memory;
+  private String type;
+  private String imageurl;
+  private Status status;
+}
